7a5a0fab0531f1dbf8e56dd40a543d1cae7c1501
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2023-01-14 20:31:47 +0530

    UC3

--- Main.java
+++ Main.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class Main  extends Details{
 	
 	
+
 	static Scanner sc = new Scanner(System.in);
 	static String firstname;
 	static String lastname;
+	static String email;
 	
 	public static void main(String[] args) {
 		System.out.println("welcome to the user registration problem");
 		userFirstname();
 		userLastname();
-
+		userEmail();
 	}
 
 	public static void userFirstname() {
@@ -25,6 +27,11 @@ public class Main  extends Details{
 		System.out.println("Enter the last Name : ");
 		lastname = sc.next();
 		Details.vaildLastName(lastname);
+	}
+	public static void userEmail() {
+		System.out.println("Enter the your Email : ");
+		email = sc.next();
+		Details.validEmain(email);
 	}
 	
 }
