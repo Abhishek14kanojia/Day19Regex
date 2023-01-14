65f9361c7ff4bf139422a49acc2a8ea47106baa8
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2023-01-14 20:28:02 +0530

    UC2

--- Details.java
+++ Details.java
@@ -4,19 +4,29 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 public class Details {
 
-	public static void ValidFirstName(String firstnsme) {
+	public static void validFirstName(String firstname) {
 		String regex = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern pt = Pattern.compile(regex);
-		Matcher mc = pt.matcher(firstnsme);
+		Matcher mc = pt.matcher(firstname);
 		
 		if(mc.matches()) {
-			System.out.println("Vaild");
+			System.out.println("valid");
 		}else {
 			System.out.println("Invalid");
 		}
 		
+	}
+	
+	public static void vaildLastName(String lastname) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pt = Pattern.compile(regex);
+		Matcher mc = pt.matcher(lastname);
 		
-		
+		if(mc.matches()) {
+			System.out.println("valid");
+		}else {
+			System.out.println("Invalid");
+		}
 		
 	}
 	
--- Main.java
+++ Main.java
@@ -7,17 +7,24 @@ public class Main  extends Details{
 	
 	static Scanner sc = new Scanner(System.in);
 	static String firstname;
+	static String lastname;
 	
 	public static void main(String[] args) {
 		System.out.println("welcome to the user registration problem");
 		userFirstname();
+		userLastname();
 
 	}
 
 	public static void userFirstname() {
 		System.out.println("Enter the first Name : ");
 		firstname = sc.next();
-		Details.ValidFirstName(firstname);
+		Details.validFirstName(firstname);
+	}
+	public static void userLastname() {
+		System.out.println("Enter the last Name : ");
+		lastname = sc.next();
+		Details.vaildLastName(lastname);
 	}
 	
 }
